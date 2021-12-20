2e5e488edb5f27a593c7a44ae6ac906379bf1b1c
kaloyanTry <kaloyantryavna@gmail.com>
2021-12-20 20:22:09 +0200

    Adding the app files on GitHub. Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+// all the varibles that will be constant in this project, so easy to config with Upercase letters that show that const is NOT change
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f6979022-8293-44b6-8eee-1b87795a4670';
+export const MODAL_CLOSE_SEC = 2.5;
